8301f46a4e0c3361cfc53b94a9572886543df969
yifan jiang <yifan99.jiang@gmail.com>
2025-08-18 14:14:12 -0700

    define publisher class

---
+++ publisher.py
@@ -0,0 +1,28 @@
+class Publisher:
+    def __init__(self):
+        self.subscribers = []
+
+    def subscribe(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def publish(self, message):
+        for subscriber in self.subscribers:
+            subscriber.receive(message)
+
+class Subscriber:
+    def __init__(self, name):
+        self.name = name
+
+    def receive(self, message):
+        print(f"{self.name}"+"received message:"
+              +f"{message}")
+
+# publisher = Publisher()
+
+# subscriber_1 = Subscriber("Subscriber 1")
+# subscriber_2 = Subscriber("Subscriber 2")
+
+# publisher.subscribe(subscriber_1)
+# publisher.subscribe(subscriber_2)
+
+# publisher.publish("Hello World")
